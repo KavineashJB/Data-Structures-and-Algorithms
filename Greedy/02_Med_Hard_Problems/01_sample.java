f8b9ca8c43a92c7469f98137d2b864e634e010e5
Kavineash JB <kavineashcsc@gmail.com>
2026-02-14 12:19:22 +0530

    Add Sample.java

---
+++ Greedy/02_Med_Hard_Problems/01_sample.java
@@ -0,0 +1,5 @@
+class Solution {
+    public static void main(String[] args) {
+        System.out.println("sample program from the new Tn govt laptop\nThis is my first push to the DSA repo");
+    }
+}
