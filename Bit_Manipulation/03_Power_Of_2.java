bb6e7a3cfc43cb0cd7edf5a8eda212b97fe685a9
Kavineash JB <kavineashcsc@gmail.com>
2026-02-22 22:37:20 +0530

    03_Power_Of_2.java

---
+++ Bit_Manipulation/03_Power_Of_2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        System.out.println("red");
+        int[] a = {1,2,3};
+        System.out.println(Arrays.toString(a));
+    }
+}
